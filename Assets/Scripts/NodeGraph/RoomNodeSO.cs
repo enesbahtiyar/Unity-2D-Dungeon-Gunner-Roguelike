78fbdbe2f4348f9a8ee8342e9a4c994bc7311190
enesbahtiyar <enesbahtiyarserce@gmail.com>
2022-12-01 23:17:01 +0300

    Editor Node

---
+++ Assets/Scripts/NodeGraph/RoomNodeSO.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomNodeSO : ScriptableObject
+{
+    [HideInInspector] public string id;
+    [HideInInspector] List<string> parentRoomNodeIDList = new List<string>();
+    [HideInInspector] List<string> childRoomNodeIDList = new List<string>();
+    [HideInInspector] public RoomNodeGraphSO roomNodeGraph;
+    public RoomNodeTypeSO roomNodeType;
+    [HideInInspector] public RoomNodeTypeListSO roomNodeTypeList;
+}
